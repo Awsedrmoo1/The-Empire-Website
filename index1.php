5b43868302d317ff3aa4ebec3d4a9d8ccacc8256
Awsedrmoo1 <144387817+Awsedrmoo1@users.noreply.github.com>
2023-09-07 16:30:45 -0400

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,155 @@
+
+
+<?php include "model/header.html"?>
+
+<section id="home">
+    <div class="section banner-main ">
+        <div class="container">
+            <div class="row marginii">
+                <div class1="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                    <div class="carousel-caption ">
+                        <h1 style="color: white; text-align:center">Welcome to</h1>
+                        <h1 style=" text-align:center"><strong class="color" >Our Store</strong></h1>
+                        <p style="margin: 30px"></p>
+                        <div style=" text-align:center"><a class="btn btn-lg btn-primary" style=" text-align:center" href="subpages/order-form.php" role="button">Order Now</a>
+                        <!--a class="btn btn-lg btn-primary" href="#about" role="button">About</a-->
+                        <a class="btn btn-lg btn-primary" href="#contact" role="button">Call Us</a></div>
+                    </div>
+                </div>
+                <div class1="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                    <div class="img-box">
+                        <img src="images/Empire_Logo.jpg" alt="Empire logo">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- plant -->
+<div id="info" class="section anchor"></div>
+<div id="plant" class="section  product">
+    <div class="container">
+        <div class="row">
+            <div class1="col-md-12 ">
+                <div class="titlepage">
+                    <h2>Store information</h2>
+
+
+                    <div style="display: table;">
+                        <div style="display: table-row;">
+                            <span style="display: table-cell; margin-top: 50px;">Hours:</span>
+                            <span>
+                                <ul style="display:table-cell; padding-left: 20px; font-size: smaller;">
+                                    <li>Sunday: 9am - 11pm</li>
+                                    <li>Monday: 9am - 11pm</li>
+                                    <li>Tuesday: Closed</li>
+                                    <li>Wednesday: 9am - 11pm</li>
+                                    <li>Thursday: 9am - 11pm</li>
+                                    <li>Friday: 9am - 11pm</li>
+                                    <li>Saturday: 9am - 11pm</li>
+                            </span>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <span>Location: <a class="contact_text1" style="padding-top: 50px;"
+                            href="https://goo.gl/maps/nf4FrWR6ERNu4fzEA" target="_blank">Independencia #2B Samaná,
+                            sanchez
+                            Dominican Republic</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<section>
+    <div id="gallery" class="section anchor"></div>
+    <div id="main_slider" class="section carousel slide banner-main" data-ride="carousel" style="height: 700px;">
+        <ol class="carousel-indicators">
+            <li data-target="#main_slider" data-slide-to="1"></li>
+            <li data-target="#main_slider" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active ">
+                <div class="container">
+                    <div class="row marginii">
+                        <div class1="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                            <div class="carousel-caption ">
+                                <h1 style="color: white;">The <strong class="color"> Gallery</strong></h1>
+                                <p>There are many variations of passages of Lorem Ipsum available, but the majority
+                                    have
+                                    suffered alteration in some form, by injected humour</p>
+                            </div>
+                        </div>
+                        <div class1="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                            <div class="img-box">
+                                <figure><img src="images/Promotional.jpg" alt="Promotional" /></figure>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="row marginii">
+                        <div class1="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                            <div class="carousel-caption ">
+                                <h1 style="color: white;">Gallery<strong class="color"> Two</strong></h1>
+                                <p>There are many variations of passages of Lorem Ipsum available, but the majority
+                                    have
+                                    suffered alteration in some form, by injected humour</p>
+                            </div>
+                        </div>
+                        <div class1="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                            <div class="img-box">
+                                <figure><img src="images/IMG_3617.jpg" alt="img" /></figure>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="row marginii">
+                        <div class=1"col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                            <div class="carousel-caption ">
+                                <h1 style="color: white;">Gallery <strong class="color"> Three</strong></h1>
+                                <p>There are many variations of passages of Lorem Ipsum available, but the majority
+                                    have
+                                    suffered alteration in some form, by injected humour</p>
+                            </div>
+                        </div>
+                        <div class=1"col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                            <div class="img-box">
+                                <figure><img src="images/Doggo.jpg" alt="img" /></figure>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+            <i class='fa fa-angle-left'></i></a>
+        <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+            <i class='fa fa-angle-right'></i>
+        </a>
+    </div>
+</section>
+<!--about -->
+<div id="about" class="section anchor"> </div>
+<div class="section about ">
+    <div class="container">
+        <div class="row">
+            <div class1="col-12">
+                <div class="titlepage">
+                    <h2><strong class="black"> About</strong> Us</h2>
+                    <span>I'm suppose to put information about who we are but at the moment I still don't know that.
+                        Maybe I can say who the owners are and put pictures but thats a lot of work and i will do it
+                        later.
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "model/footer.htm"?>
